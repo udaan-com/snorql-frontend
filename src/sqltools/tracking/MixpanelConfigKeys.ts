a57e2c5b5d5e657cd7f835462f2e3a5c57ff728a
Lokesh Devnani <lokesh.devnani@udaan.com>
2023-05-01 13:23:40 +0530

    Trimmed down unused packages, added proxy dep+url

--- src/sqltools/tracking/MixpanelConfigKeys.ts
+++ src/sqltools/tracking/MixpanelConfigKeys.ts
@@ -1,4 +1,4 @@
 // let env_check = window.location.href.includes('https://tools.udaan.dev/')
 // let mixpanelToken = 'cf64b7af989cc59c53df9013cbb12a77'
-export let env_check = window.location.href.includes('http://localhost:7060/') // use this for local testing
+export let env_check = window.location.href.includes('http://localhost:3000/') // use this for local testing
 export let mixpanelToken = '8d852d8b875461381db5973372045141'; // use this for local testing
